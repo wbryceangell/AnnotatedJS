e33aac6b14b6bff8b89fb3476b73a64f6d4b796f
William Angell <wbryceangell@gmail.com>
2023-01-23 16:15:08 -0600

    do away with subrouters

--- src/router.ts
+++ src/router.ts
@@ -2,25 +2,24 @@ import { WorkerRouter } from "@worker-tools/router";
 import { getControllers } from "./decorators/class/controller";
 import { getKey, methodKeys } from "./decorators/keys";
 import getMethods from "./decorators/method/getMethods";
+import normalizePath from "normalize-path";
 
 let router = new WorkerRouter();
 
 for (const controller of getControllers()) {
-  let controllerRouter = new WorkerRouter();
   for (const methodKey of methodKeys) {
     const methods = getMethods(methodKey, controller.constructor.prototype);
     switch (methodKey) {
       case getKey: {
         for (const method of methods) {
-          controllerRouter = controllerRouter.get(
-            method.path,
+          router = router.get(
+            normalizePath([controller.path, method.path].join("/")),
             controller.constructor.prototype[method.property]
           );
         }
       }
     }
   }
-  router = router.use(controller.path, controllerRouter);
 }
 
 router = router.recover("*", (req) => {
