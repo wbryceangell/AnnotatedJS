149cf463e570efac5943ce3c4035e291ea761b27
William Bryce Angell <bryce.angell@resi.io>
2024-01-16 09:29:37 -0600

    remove functional testing

--- test/functional/app/sw.ts
+++
@@ -1,9 +0,0 @@
-import { initialize } from "@src";
-import "./serviceWorkerConfig";
-import "./controllers/storageController";
-
-const requestHandler = initialize();
-const eventHandler = (evt: Event) => {
-  (<FetchEvent>evt).respondWith(requestHandler((<FetchEvent>evt).request));
-};
-addEventListener("fetch", eventHandler);
