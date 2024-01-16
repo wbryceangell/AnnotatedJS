d1b4fdc41635a91972a0fe644a7fd78999a70bf1
William Bryce Angell <bryce.angell@resi.io>
2024-01-15 20:03:00 -0600

    finish initialization integration tests

--- test/integration/initialize.test.ts
+++ test/integration/initialize.test.ts
@@ -1,7 +1,17 @@
-import { initialize } from "../../src/index";
+import { Config, initialize } from "../../src/index";
 
 describe("Initialization", () => {
   it("fails when there isn't a config", () => {
     expect(() => initialize()).toThrow();
+  });
+
+  it("should work when router is configured", () => {
+    @Config
+    class Configuration {
+      getRouter() {
+        return {};
+      }
+    }
+    expect(() => initialize()).not.toThrow();
   });
 });
