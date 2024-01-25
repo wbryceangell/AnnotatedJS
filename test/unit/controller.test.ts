59de1da583a03490ee56c88af16db60c1fc6d930
William Bryce Angell <bryce.angell@resi.io>
2024-01-25 09:21:06 -0600

    create instance of controller during initialization

--- test/unit/controller.test.ts
+++ test/unit/controller.test.ts
@@ -1,7 +1,7 @@
 import { Controller } from "../../src/index";
 
 describe("Controller", () => {
-  it("creates an instance of a class", () => {
+  it("creates an instance of the controller class", () => {
     const spy = jest.fn();
 
     Controller("path", { Router: {} })(
@@ -31,6 +31,27 @@ describe("Controller", () => {
       metadata: {},
     });
 
-    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith(expect.any(Function));
+  });
+
+  it("initialization hook to create instance of class", () => {
+    const spy = jest.fn();
+
+    class ControllerClass {
+      constructor() {
+        spy();
+      }
+    }
+
+    Controller("path", { Router: {} })(ControllerClass, {
+      kind: "class",
+      name: "Controller",
+      addInitializer: jest.fn((initializer: Function) => {
+        initializer.call(ControllerClass);
+      }),
+      metadata: {},
+    });
+
+    expect(spy).toHaveBeenCalledTimes(2);
   });
 });
