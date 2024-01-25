d7fa7f28e922086bf8fbe88d94e65fda08adbcc5
William Bryce Angell <bryce.angell@resi.io>
2024-01-25 08:51:17 -0600

    instantiate controller in decorator

--- src/decorators/router.ts
+++ src/decorators/router.ts
@@ -2,25 +2,30 @@ import { container as defaultContainer } from "../container/container";
 import { setRouter } from "../container/utils/setRouter";
 import { validateContainer } from "../container/utils/validateContainer";
 import { setInjectables } from "./inject/setInjectables";
+import { AnnotatedRouter } from "./types";
 import { getMetadata } from "./utils/getMetadata";
 import { validateKind } from "./utils/validateKind";
 
 export const Router =
-  (container = defaultContainer) =>
-  (constructor: NewableFunction, context: ClassDecoratorContext) => {
+  <
+    Class extends new (...args: unknown[]) => AnnotatedRouter = new (
+      ...args: unknown[]
+    ) => AnnotatedRouter
+  >(
+    container = defaultContainer
+  ) =>
+  (constructor: Class, context: ClassDecoratorContext<Class>) => {
     validateContainer(container);
 
     const annotationName = `@${Router.name}`;
     validateKind(annotationName, context, "class");
 
     context.addInitializer(function () {
-      // @ts-expect-error it does not think that this is newable
       const router = new this();
       const metadata = getMetadata(annotationName, context);
       setInjectables(container, router, metadata);
       setRouter(container, router);
     });
 
-    // @ts-expect-error NewableFunction type does not include new for some reason
     new constructor();
   };
