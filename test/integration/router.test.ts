318143940385a53ecc13eca9f02edc1422bc0806
William Bryce Angell <bryce.angell@resi.io>
2024-01-31 07:54:24 -0600

    add router handle integration test

--- test/integration/router.test.ts
+++ test/integration/router.test.ts
@@ -1,4 +1,5 @@
-import { Router } from "../../src";
+import { AnnotatedRouter, Router } from "../../src";
+import { RequestHandler } from "../../src/decorators/types";
 
 describe("Router", () => {
   it("should work when it is used on a class", () => {
@@ -7,5 +8,44 @@ describe("Router", () => {
       @Router({})
       class TestRouter {}
     }).not.toThrow();
+  });
+
+  it("handles a request", async () => {
+    const expectedBody = "body";
+    const container: { Router?: TestRouter } = {};
+
+    @Router(container)
+    class TestRouter implements AnnotatedRouter {
+      get(uri: string, handler: RequestHandler): AnnotatedRouter {
+        throw new Error("Method not implemented.");
+      }
+      put(uri: string, handler: RequestHandler): AnnotatedRouter {
+        throw new Error("Method not implemented.");
+      }
+      post(uri: string, handler: RequestHandler): AnnotatedRouter {
+        throw new Error("Method not implemented.");
+      }
+      patch(uri: string, handler: RequestHandler): AnnotatedRouter {
+        throw new Error("Method not implemented.");
+      }
+      delete(uri: string, handler: RequestHandler): AnnotatedRouter {
+        throw new Error("Method not implemented.");
+      }
+      all(uri: string, handler: RequestHandler): AnnotatedRouter {
+        throw new Error("Method not implemented.");
+      }
+      async handle(req: Request): Promise<Response> {
+        return new Response(expectedBody);
+      }
+    }
+
+    const response = (await container.Router?.handle(
+      new Request("http://localhost")
+    )) as Response;
+
+    expect(response).toBeDefined();
+
+    const body = await response.text();
+    expect(body).toBe(expectedBody);
   });
 });
