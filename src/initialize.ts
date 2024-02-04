c6fd9f7d68e10525dc491e6e1f11015cc63a3c98
William Bryce Angell <bryce.angell@resi.io>
2024-02-04 09:31:17 -0600

    initialize instantiates config

--- src/initialize.ts
+++ src/initialize.ts
@@ -1,7 +1,16 @@
 import { container as defaultContainer } from "./container/container";
+import { keys } from "./container/keys";
+import { getGlobal } from "./container/utils/getGlobal";
 import { getRouter } from "./container/utils/getRouter";
+import { Class } from "./decorators/types";
 
 export const initialize = (container = defaultContainer) => {
+  for (const config of <Array<Class<unknown>>>(
+    getGlobal(container, keys.configs)
+  )) {
+    new config();
+  }
+
   const router = getRouter(container);
   return router.handle.bind(router);
 };
