c45c2b63c860942499db0d3538b7ecc4fdfe8d57
William Bryce Angell <bryce.angell@resi.io>
2024-02-05 11:14:38 -0600

    set router class in container in @Router

---
+++ test/unit/router.test.ts
@@ -0,0 +1,52 @@
+import { Router } from "../../src";
+import { keys } from "../../src/container/keys";
+
+describe("@Router", () => {
+  it("adds class to container", () => {
+    const kind = "class";
+    const name = "Router";
+    class RouterClass {}
+    const container = {};
+
+    Router(container)(
+      // @ts-expect-error Class type is too broad for anonymous class
+      RouterClass,
+      {
+        kind,
+        name,
+        addInitializer: () => {},
+        metadata: {},
+      }
+    );
+
+    expect(container[keys.routerClass]).toBeDefined();
+  });
+
+  it("allows setting only one router", () => {
+    const kind = "class";
+    const name = "Router";
+    const addInitializer = () => {};
+    const metadata = {};
+    class RouterClass {}
+    const container = {};
+
+    Router(container)(
+      // @ts-expect-error Class type is too broad for anonymous class
+      RouterClass,
+      {
+        kind,
+        name,
+        addInitializer,
+        metadata,
+      }
+    );
+
+    expect(() =>
+      Router(container)(
+        // @ts-expect-error Class type is too broad for anonymous class
+        RouterClass,
+        { kind, name, addInitializer, metadata }
+      )
+    ).toThrow();
+  });
+});
