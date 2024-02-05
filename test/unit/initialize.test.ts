e764fd1565378221b12cd1720e260681cd2960c2
William Bryce Angell <bryce.angell@resi.io>
2024-02-05 08:44:04 -0600

    refactor initialize tests

--- test/unit/initialize.test.ts
+++ test/unit/initialize.test.ts
@@ -7,98 +7,33 @@ describe("initialize", () => {
   }
   const router = new Router();
 
-  it("does not error if there are no config classes", () => {
-    expect(() =>
-      initialize({ [keys.router]: router, [keys.routerClass]: Router })
-    ).not.toThrow();
-  });
-
-  it("instantiates a config class", () => {
-    const spy = jest.fn();
-    class Config {
-      constructor() {
-        spy();
+  const itInstantiatesMultipleClasses = (key: string) =>
+    it(`instantiates multiple ${key}`, () => {
+      const spy = jest.fn();
+      class ClassOne {
+        constructor() {
+          spy(this);
+        }
       }
-    }
+      class ClassTwo {
+        constructor() {
+          spy(this);
+        }
+      }
 
-    initialize({
-      [keys.configClasses]: [Config],
-      [keys.routerClass]: Router,
-      [keys.router]: router,
+      initialize({
+        [key]: [ClassOne, ClassTwo],
+        [keys.routerClass]: Router,
+        [keys.router]: router,
+      });
+
+      expect(spy).toHaveBeenNthCalledWith(1, expect.any(ClassOne));
+      expect(spy).toHaveBeenNthCalledWith(2, expect.any(ClassTwo));
     });
 
-    expect(spy).toHaveBeenCalled();
-  });
-
-  it("instantiates multiple config classes", () => {
-    const spy = jest.fn();
-    class ConfigOne {
-      constructor() {
-        spy(this);
-      }
-    }
-    class ConfigTwo {
-      constructor() {
-        spy(this);
-      }
-    }
-
-    initialize({
-      [keys.configClasses]: [ConfigOne, ConfigTwo],
-      [keys.routerClass]: Router,
-      [keys.router]: router,
-    });
-
-    expect(spy).toHaveBeenNthCalledWith(1, expect.any(ConfigOne));
-    expect(spy).toHaveBeenNthCalledWith(2, expect.any(ConfigTwo));
-  });
-
-  it("instantiates multiple service classes", () => {
-    const spy = jest.fn();
-    class ServiceOne {
-      constructor() {
-        spy(this);
-      }
-    }
-    class ServiceTwo {
-      constructor() {
-        spy(this);
-      }
-    }
-
-    initialize({
-      [keys.serviceClasses]: [ServiceOne, ServiceTwo],
-      [keys.routerClass]: Router,
-      [keys.router]: router,
-    });
-
-    expect(spy).toHaveBeenNthCalledWith(1, expect.any(ServiceOne));
-    expect(spy).toHaveBeenNthCalledWith(2, expect.any(ServiceTwo));
-  });
-
-  it("instantiates configs before services", () => {
-    const spy = jest.fn();
-    class Config {
-      constructor() {
-        spy(this);
-      }
-    }
-    class Service {
-      constructor() {
-        spy(this);
-      }
-    }
-
-    initialize({
-      [keys.configClasses]: [Config],
-      [keys.serviceClasses]: [Service],
-      [keys.routerClass]: Router,
-      [keys.router]: router,
-    });
-
-    expect(spy).toHaveBeenNthCalledWith(1, expect.any(Config));
-    expect(spy).toHaveBeenNthCalledWith(2, expect.any(Service));
-  });
+  itInstantiatesMultipleClasses(keys.configClasses);
+  itInstantiatesMultipleClasses(keys.serviceClasses);
+  itInstantiatesMultipleClasses(keys.controllerClasses);
 
   it("errors if a router class is not in the container", () => {
     expect(() =>
@@ -122,26 +57,40 @@ describe("initialize", () => {
     expect(spy).toHaveBeenCalled();
   });
 
-  it("instantiates multiple controller classes", () => {
+  it("instantiates in order", () => {
     const spy = jest.fn();
-    class ControllerOne {
+    class Config {
       constructor() {
         spy(this);
       }
     }
-    class ControllerTwo {
+    class Service {
+      constructor() {
+        spy(this);
+      }
+    }
+    class Router {
+      constructor() {
+        spy(this);
+      }
+    }
+    class Controller {
       constructor() {
         spy(this);
       }
     }
 
     initialize({
-      [keys.controllerClasses]: [ControllerOne, ControllerTwo],
+      [keys.configClasses]: [Config],
+      [keys.serviceClasses]: [Service],
       [keys.routerClass]: Router,
+      [keys.controllerClasses]: [Controller],
       [keys.router]: router,
     });
 
-    expect(spy).toHaveBeenNthCalledWith(1, expect.any(ControllerOne));
-    expect(spy).toHaveBeenNthCalledWith(2, expect.any(ControllerTwo));
+    expect(spy).toHaveBeenNthCalledWith(1, expect.any(Config));
+    expect(spy).toHaveBeenNthCalledWith(2, expect.any(Service));
+    expect(spy).toHaveBeenNthCalledWith(3, expect.any(Router));
+    expect(spy).toHaveBeenNthCalledWith(4, expect.any(Controller));
   });
 });
