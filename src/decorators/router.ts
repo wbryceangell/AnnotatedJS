644aba81e361b50c0af446bd6aaabe9ac9dec5ba
William Bryce Angell <bryce.angell@resi.io>
2024-02-09 09:02:37 -0600

    add @Router TSDoc

--- src/decorators/router.ts
+++ src/decorators/router.ts
@@ -7,9 +7,60 @@ import { AnnotatedRouter, Class, ClassDecorator } from "./types";
 import { getMetadata } from "./utils/getMetadata";
 import { validateKind } from "./utils/validateKind";
 
-export const Router = <T extends Class<AnnotatedRouter>>(
-  container = defaultContainer,
-) =>
+/**
+ * A class decorator that defines the router used by controllers
+ *
+ * @remarks
+ *
+ * `@Router` annotates the class that will handle incoming requests
+ *
+ * The class should implement the `AnnotatedRouter` interface. It is a [Fluent interface](https://en.wikipedia.org/wiki/Fluent_interface)
+ *
+ * @example
+ * ```ts
+ * import { AnnotatedRouter, Router } from "@fork-git-it/annotatedjs";
+ *
+ * @Router()
+ * export class ExampleRouter implements AnnotatedRouter {
+ *   get(uri: string, handler: RequestHandler): AnnotatedRouter {
+ *     // configure router to handle GET request
+ *     return this;
+ *   }
+ *
+ *   put(uri: string, handler: RequestHandler): AnnotatedRouter {
+ *     // configure router to handle PUT request
+ *     return this;
+ *   }
+ *
+ *   post(uri: string, handler: RequestHandler): AnnotatedRouter {
+ *     // configure router to handle POST request
+ *     return this;
+ *   }
+ *
+ *   patch(uri: string, handler: RequestHandler): AnnotatedRouter {
+ *     // configure router to handle PATCH request
+ *     return this;
+ *   }
+ *
+ *   delete(uri: string, handler: RequestHandler): AnnotatedRouter {
+ *     // configure router to handle DELETE request
+ *     return this;
+ *   }
+ *
+ *   all(uri: string, handler: RequestHandler): AnnotatedRouter {
+ *     // configure router to handle all requests
+ *     return this;
+ *   }
+ *
+ *   handle(req: Request): Promise<Response> {
+ *     // handle incoming request using the router
+ *   }
+ * }
+ * ```
+ *
+ * @param container - Object that stores injectables
+ */
+export const Router = (container = defaultContainer) =>
   ((constructor, context) => {
     validateContainer(container);
 
@@ -24,4 +75,4 @@ export const Router = <T extends Class<AnnotatedRouter>>(
     });
 
     setGlobal(container, keys.routerClass, constructor);
-  }) as ClassDecorator<T>;
+  }) as ClassDecorator<Class<AnnotatedRouter>>;
