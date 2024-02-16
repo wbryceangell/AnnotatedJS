84194fd87926a240264348d99c05e7b2c783ad2b
William Bryce Angell <bryce.angell@resi.io>
2024-02-16 14:07:35 -0600

    Cache errors when @CacheStorage is not used on class

--- test/integration/cache.test.ts
+++ test/integration/cache.test.ts
@@ -3,11 +3,21 @@
 import { CacheStorage } from "../../src";
 
 describe("Cache", () => {
-  it("cache storage errors with invalid container", () => {
+  it("errors when @CacheStorage is used with invalid container", () => {
     expect(() => {
       // @ts-expect-error testing invalid container
       @CacheStorage(null)
       class TestCacheStorage {}
+    }).toThrow();
+  });
+
+  it("error when @CacheStorage is used on something other than a class", () => {
+    expect(() => {
+      class TestClass {
+        // @ts-expect-error using on something other than a class
+        @CacheStorage({})
+        notClass() {}
+      }
     }).toThrow();
   });
 });
