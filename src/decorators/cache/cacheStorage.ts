127d900e58b192224c09f3b93936fb5625cb5ba9
William Bryce Angell <bryce.angell@resi.io>
2024-02-16 15:02:20 -0600

    add @CacheStorage documentation

--- src/decorators/cache/cacheStorage.ts
+++ src/decorators/cache/cacheStorage.ts
@@ -8,6 +8,36 @@ import { Class, ClassDecorator } from "../types";
 import { getMetadata } from "../utils/getMetadata";
 import { validateKind } from "../utils/validateKind";
 
+/**
+ * A class decorator that defines the global cache store
+ *
+ * @see
+ *
+ * {@link AnnotatedCacheStorage} for interface definition
+ *
+ * @example
+ * ```ts
+ * import { AnnotatedCacheStorage, CacheStorage } from "@fork-git-it/annotatedjs";
+ *
+ * @CacheStorage()
+ * export class ExampleCacheStorage implements AnnotatedCacheStorage {
+ *   has(cacheName: string) {
+ *     // caches is the global CacheStorage in browser runtimes
+ *     return caches.has(cacheName);
+ *   }
+ *
+ *   open(cacheName: string) {
+ *     return caches.open(cacheName);
+ *   }
+ *
+ *   delete(cacheName: string) {
+ *     return caches.delete(cacheName);
+ *   }
+ * }
+ * ```
+ *
+ * @param container - Object that stores injectables
+ */
 export const CacheStorage = (container = defaultContainer) =>
   ((constructor, context) => {
     validateContainer(container);
