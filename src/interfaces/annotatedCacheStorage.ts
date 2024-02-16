83376f834b77e2ee8b83571d4f4cae7dfb0ee0b2
William Bryce Angell <bryce.angell@resi.io>
2024-02-16 08:22:38 -0600

    add usage docs for AnnotatedCacheStorage

--- src/interfaces/annotatedCacheStorage.ts
+++ src/interfaces/annotatedCacheStorage.ts
@@ -1,5 +1,10 @@
 import { Cache } from "./cache";
 
+/**
+ * @see
+ *
+ * {@link https://developer.mozilla.org/en-US/docs/Web/API/CacheStorage | MDN} for spec
+ */
 export interface AnnotatedCacheStorage {
   has(cacheName: string): Promise<boolean>;
   open(cacheName: string): Promise<Cache>;
