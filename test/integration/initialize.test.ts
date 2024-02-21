98474fc1fac18b5b995dded42c4cffaef4e8fe33
Kevin Won <kevin.won91@gmail.com>
2024-02-21 14:31:17 -0600

    add integration test for double initilization of initialize.ts | change json test to run jest synchornously '&&'

--- test/integration/initialize.test.ts
+++ test/integration/initialize.test.ts
@@ -185,4 +185,35 @@ describe("Initialization", () => {
     expect(putResponse).toBeDefined();
     expect(putResponse.status).toBe(204);
   });
+  it("Initialize twice expects returns error on second init", () =>{
+    const container = {};
+
+    @Router(container)
+    class TestRouter implements AnnotatedRouter {
+      get(_uri: string, _handler: RequestHandler): AnnotatedRouter {
+        throw new Error("Method not implemented.");
+      }
+      put(_uri: string, _handler: RequestHandler): AnnotatedRouter {
+        throw new Error("Method not implemented.");
+      }
+      post(_uri: string, _handler: RequestHandler): AnnotatedRouter {
+        throw new Error("Method not implemented.");
+      }
+      patch(_uri: string, _handler: RequestHandler): AnnotatedRouter {
+        throw new Error("Method not implemented.");
+      }
+      delete(_uri: string, _handler: RequestHandler): AnnotatedRouter {
+        throw new Error("Method not implemented.");
+      }
+      all(_uri: string, _handler: RequestHandler): AnnotatedRouter {
+        throw new Error("Method not implemented.");
+      }
+      handle(_req: Request): Promise<Response> {
+        throw new Error("Method not implemented.");
+      }
+    }
+
+    initialize(container);
+    expect(() => initialize(container)).toThrow;
+  });
 });
