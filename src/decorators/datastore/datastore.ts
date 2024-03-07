fd5b4cb7800bcd64ff2505683d43053534c30f35
William Bryce Angell <bryce.angell@resi.io>
2024-03-07 10:55:42 -0600

    add datastore documentation

--- src/decorators/datastore/datastore.ts
+++ src/decorators/datastore/datastore.ts
@@ -8,6 +8,62 @@ import { getStandardInitializer } from "../utils/getStandardInitializer";
 import { validateKind } from "../utils/validateKind";
 import { validateName } from "../utils/validateName";
 
+/**
+ * A class decorator that defines an injectable datastore
+ *
+ * @see
+ *
+ * {@link AnnotatedDatastore} for interface definition
+ *
+ * @example
+ * ```ts
+ * import { AnnotatedDatastore, Datastore } from "@fork-git-it/annotatedjs";
+ *
+ * @Datastore()
+ * class TestDatastore implements AnnotatedDatastore<string> {
+ *   private map = new Map<string, string>();
+ *
+ *   length: number = 0;
+ *
+ *   async clear(): Promise<undefined> {
+ *     this.map.clear();
+ *     this.length = 0;
+ *   }
+ *
+ *   async getItem(keyName: string): Promise<string | null> {
+ *     return this.map.get(keyName) || null;
+ *   }
+ *
+ *   async key(index: number): Promise<string | null> {
+ *     return [...this.map.keys()].at(index) || null;
+ *   }
+ *
+ *   async removeItem(keyName: string): Promise<undefined> {
+ *     const keyExisted = this.map.has(keyName);
+ *
+ *     this.map.delete(keyName);
+ *
+ *     if (!keyExisted) {
+ *       this.length--;
+ *     }
+ *   }
+ *
+ *   async setItem(keyName: string, value: string): Promise<undefined> {
+ *     const keyExisted = this.map.has(keyName);
+ *
+ *     this.map.set(keyName, value);
+ *
+ *     if (!keyExisted) {
+ *       this.length++;
+ *     }
+ *   }
+ * }
+ * ```
+ *
+ * @typeParam T - Type of the datastore values
+ *
+ * @param container - Object that stores injectables
+ */
 export const Datastore = <T>(container = defaultContainer) =>
   ((constructor, context) => {
     validateContainer(container);
