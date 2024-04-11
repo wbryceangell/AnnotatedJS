e4fb8ef5f3bccfba890ef130419ed3a7b1f35a97
Bryce Angell <devdecaf@gmail.com>
2024-04-11 13:17:24 +0000

    request scope must be used on an existing config

--- src/decorators/config/requestScope.ts
+++ src/decorators/config/requestScope.ts
@@ -1,16 +1,30 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import { container as defaultContainer } from "../../container/container";
+import { keys } from "../../container/keys";
+import { getGlobal } from "../../container/utils/getGlobal";
 import { validateContainer } from "../../container/utils/validateContainer";
 import { Class, ClassDecorator } from "../types";
 import { validateKind } from "../utils/validateKind";
+import { validateName } from "../utils/validateName";
 
 export const RequestScope = <T extends Class<object>>(
   container: Record<string, Array<T>> = defaultContainer,
 ) =>
-  ((_constructor, context) => {
+  ((constructor, context) => {
     validateContainer(container);
 
     const annotationName = `@${RequestScope.name}`;
     validateKind(annotationName, context, "class");
+    validateName(annotationName, context);
+
+    const configClasses = getGlobal(container, keys.configClasses);
+    if (
+      !Array.isArray(configClasses) ||
+      !configClasses.find((config) => config.name === constructor.name)
+    ) {
+      throw new Error(
+        `${annotationName} must be used on an existing configuration class`,
+      );
+    }
   }) as ClassDecorator<T>;
--- test/unit/requestScope.test.ts
+++ test/unit/requestScope.test.ts
@@ -1,10 +1,29 @@
+import { keys } from "../../src/container/keys";
 import { RequestScope } from "../../src/decorators/config/requestScope";
 import {
   itExpectsAValidContainer,
   itThrowsErrorIfNotUsedOnAClass,
+  itThrowsWhenUsedOnAnUnnamedClass,
 } from "./utils/classDecorators";
 
 describe("@RequestScope", () => {
   itExpectsAValidContainer(RequestScope);
   itThrowsErrorIfNotUsedOnAClass(RequestScope);
+  itThrowsWhenUsedOnAnUnnamedClass(RequestScope);
+
+  it("throws error when class is not a config class in the container", () => {
+    class Config {}
+    class OtherClass {}
+
+    expect(() =>
+      RequestScope({ [keys.configClasses]: [Config] })(
+        OtherClass,
+        // @ts-expect-error not a complete class decorator context
+        {
+          kind: "class",
+          name: OtherClass.name,
+        },
+      ),
+    ).toThrow();
+  });
 });
